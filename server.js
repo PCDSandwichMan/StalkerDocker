f04535d515178ebfca12bbb8911af080b389b719
Matt <matthew.endicott46@gmail.com>
2019-08-22 08:39:39 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const app = express();
+
+const indexRouter = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true });
+
+app.use('/', indexRouter);
+
+app.listen(config.PORT);
+
+const db = mongoose.connection;
+
+db.on('error', err => console.log(err));
+
+db.once('open', () => {
+  console.log(`App running on Port: ${config.PORT}`);
+});
