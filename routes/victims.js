9e2fde4d830236f27120544f86fa9846deec429e
Matt <matthew.endicott46@gmail.com>
2019-08-24 17:38:36 -0600

    inital victims routes

--- routes/victims.js
+++ routes/victims.js
@@ -3,8 +3,22 @@ const router = express.Router();
 const Victim = require('../models/Victim');
 
 //  All victim's
-router.get('/', (req, res) => {
-  res.render('victims/index');
+router.get('/', async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== '') {
+    searchOptions.name = await new RegExp(req.query.name, 'i');
+  } 
+  
+  try {
+    const victims = await Victim.find(searchOptions);
+    res.render('victims/index', {
+      victims: victims,
+      searchOptions: req.query.name
+    });
+  } catch {
+    res.redirect('/');
+    err => console.log(`get / error ${err}`);
+  }
 });
 
 // new victim
@@ -16,17 +30,27 @@ router.get('/new', (req, res) => {
 
 // Create Victim Route
 router.post('/', async (req, res) => {
-  const victim = new Victim({
-    name: req.body.name
-  });
+  const existing = await Victim.find({ name: req.body.name });
+  if (existing.length >= 1) {
+    return res.render('victims/new', {
+      errorMessage:
+        "Dude NO double tracking on a single victim. That's just creepy.....weirdo......"
+    });
+  }
+
   try {
-    const newVictim = await victim.save();
+    new Victim({
+      name: req.body.name
+    })
+      .save()
+      .then()
+      .catch(err => console.log('something went down in the "/" post ===> ' + err));
     // res.redirect(`victims/${result.id}`)
-    res.redirect(`victims`);
+    res.redirect('/victims');
   } catch {
     err => {
       res.render('victims/new', {
-        victim: newVictim,
+        victim: victim,
         errorMessage:
           'Unable to track ninjas until version 10.3.2.43.2234.21 (exactly...no sooner....no later....)'
       });
